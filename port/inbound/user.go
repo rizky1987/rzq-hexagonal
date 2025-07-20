b2a4706c2408e47f00b3756a213ed034b2fe9884
RizkyMSoleh <rizky.msoleh@gmail.com>
2025-07-20 18:56:23 +0700

    add inbound user

--- port/inbound/user.go
+++ port/inbound/user.go
@@ -1,4 +1,10 @@
 package inbound
 
+import (
+	"rzq-hexagonal/adapter/http/request"
+	"rzq-hexagonal/domain/entity"
+)
+
 type UserInbound interface {
+	Register(req request.RegisterRequest) (*entity.User, error)
 }
