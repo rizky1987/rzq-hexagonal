73545d6d88d1ee59c00e0862aa1bff09e8c66d76
RizkyMSoleh <rizky.msoleh@gmail.com>
2025-07-20 19:05:21 +0700

    add reposirory database implementation

--- domain/repository/postgres/user_repository.go
+++ domain/repository/postgres/user_repository.go
@@ -1,1 +1,25 @@
 package repository
+
+import (
+	"rzq-hexagonal/domain/entity"
+	outboundInterface "rzq-hexagonal/port/outbound"
+
+	"gorm.io/gorm"
+)
+
+type UserRepositoryPostgreImpl struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) outboundInterface.UserRepository {
+	return &UserRepositoryPostgreImpl{db: db}
+}
+
+func (r *UserRepositoryPostgreImpl) Register(user *entity.User) (*entity.User, error) {
+
+	err := r.db.Save(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
