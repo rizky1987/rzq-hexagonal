904741a191dd89c7b009c9a527c8ad99646fcf0b
RizkyMSoleh <rizky.msoleh@gmail.com>
2025-07-20 18:40:20 +0700

    create new entity

--- domain/entity/user.go
+++ domain/entity/user.go
@@ -1,1 +1,22 @@
 package entity
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	Id    uuid.UUID `gorm:"column:Id;type:uuid;primaryKey"`
+	Name  string    `gorm:"column:Name;type:string;primaryKey"`
+	Email string    `gorm:"column:Email;type:string;uniqueIndex"`
+}
+
+// fungsi digunakan untuk membuat logic tambahan sebelum kita menyimpan
+// data ke database. Dicontoh ini saya hanya mencontohkan jika developer
+// lupa menambahkan data Id, maka disini Id akan diisi dengan uuid.New()
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	if u.Id == uuid.Nil {
+		u.Id = uuid.New()
+	}
+	return
+}
