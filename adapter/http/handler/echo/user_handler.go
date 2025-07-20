b53344f742c08b06e6f5ece3677451dfa1d96d26
RizkyMSoleh <rizky.msoleh@gmail.com>
2025-07-20 19:06:19 +0700

    add handler

--- adapter/http/handler/echo/user_handler.go
+++ adapter/http/handler/echo/user_handler.go
@@ -1,9 +1,31 @@
 package handler
 
 import (
+	"net/http"
+	"rzq-hexagonal/adapter/http/request"
+	"rzq-hexagonal/adapter/http/response"
 	"rzq-hexagonal/infrastructure/factory"
+
+	"github.com/labstack/echo/v4"
 )
 
 type EchoHandler struct {
 	ServicesFactory *factory.ServicesFactory
+}
+
+func (h *EchoHandler) Register(c echo.Context) error {
+
+	var req request.RegisterRequest
+	if err := c.Bind(&req); err != nil {
+		return err
+	}
+
+	user, err := h.ServicesFactory.UserUseCase.Register(req)
+	if err != nil {
+		return c.JSON(http.StatusUnauthorized, err.Error())
+	}
+
+	result := response.ConvertUserFromEntity(user)
+
+	return c.JSON(http.StatusOK, result)
 }
